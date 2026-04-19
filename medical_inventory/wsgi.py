5e52eefdcfd474d1209cb1779f111a6a33a4ced6
Medical Inventory Admin <admin@medicalinventory.com>
2026-04-19 17:25:08 +0530

    Use production settings for Railway deployment WSGI startup

--- medical_inventory/wsgi.py
+++ medical_inventory/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_inventory.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_inventory.settings_prod')
 
 application = get_wsgi_application()
